3ac56b1ae9f38a905f23920f246a350c468e9b81
Hugo Watzl <143032400+HugoWatzl@users.noreply.github.com>
2023-09-01 19:37:32 -0300

    Add files via upload

---
+++ ac2ibemec.py
@@ -0,0 +1,19 @@
+
+#questão 1 ac2
+
+def salario( horas , numero ):
+
+    num = horas * numero 
+    return num 
+
+horas = float(input("salario por hora de :"))
+numero = float(input("numero de horas trabalhadas no mes foi de :"))
+
+num2 = salario( horas , numero )
+print("o salario a receber e de :" , num2 )
+        
+##########################################################################
+
+
+
+#questao 2 ac2
